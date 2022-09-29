09c3a4db68a61e89a9f08fd682f8c301ca5931e8
gitpk-0 <kell108p@protonmail.com>
2022-09-29 19:30:01 -0400

    navbar

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -26,6 +26,7 @@ import about04 from "../assets/about04.png";
 import profile from "../assets/profile.png";
 import circle from "../assets/circle.svg";
 import logo from "../assets/logo.png";
+import patrickKellLogo from "../assets/patrickKellLogo.png";
 
 import adidas from "../assets/adidas.png";
 import amazon from "../assets/amazon.png";
@@ -62,6 +63,7 @@ export default {
   profile,
   circle,
   logo,
+  patrickKellLogo,
   adidas,
   amazon,
   asus,
