5e9bc05e1ef19b232f5557c24be9eb6d89aef9c9
gitpk-0 <kell108p@protonmail.com>
2022-09-30 20:19:10 -0400

    about section

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -33,7 +33,7 @@ const Header = () => {
           </div>
 
           <div className="tag-cmp app__flex">
-            <p className="p-text">Software Developer</p>
+            {/* <p className="p-text">Software Developer</p> */}
             <p className="p-text">Web Developer</p>
           </div>
         </div>
